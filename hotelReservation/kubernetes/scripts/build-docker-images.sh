bd4ffbab5b12c7c0083fba956ba43e0bc1263a44
Alfred Daimari <alfred_daimari@yahoo.com>
2024-09-20 17:24:21 +0200

    Update build-docker-images.sh
    load the image into local docker

--- hotelReservation/kubernetes/scripts/build-docker-images.sh
+++ hotelReservation/kubernetes/scripts/build-docker-images.sh
@@ -12,7 +12,7 @@ do
   IMAGE=${i}
   echo Processing image ${IMAGE}
   cd $ROOT_FOLDER
-  $EXEC build -t "$USER"/"$IMAGE":"$TAG" -f Dockerfile . --platform linux/amd64
+  $EXEC build -t "$USER"/"$IMAGE":"$TAG" -f Dockerfile . --platform linux/amd64 --load
   cd $ROOT_FOLDER
   echo
 done
