6bdc3d99cbcaf5ae46dac634bb0c47e99270ab8b
Alfred Daimari <alfred_daimari@yahoo.com>
2024-09-20 17:17:15 +0200

    Update build-docker-images.sh
    remove arm64 build

--- hotelReservation/kubernetes/scripts/build-docker-images.sh
+++ hotelReservation/kubernetes/scripts/build-docker-images.sh
@@ -12,7 +12,7 @@ do
   IMAGE=${i}
   echo Processing image ${IMAGE}
   cd $ROOT_FOLDER
-  $EXEC build -t "$USER"/"$IMAGE":"$TAG" -f Dockerfile . --platform linux/arm64,linux/amd64 --push
+  $EXEC build -t "$USER"/"$IMAGE":"$TAG" -f Dockerfile . --platform linux/amd64 --push
   cd $ROOT_FOLDER
   echo
 done
